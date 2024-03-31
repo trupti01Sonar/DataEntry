265179a11ab6a0f5f96bc1f8e8483f67fdf8c54c
trupti01Sonar <164472300+trupti01Sonar@users.noreply.github.com>
2024-03-31 20:06:02 +0530

    Add files via upload

---
+++ reg1.js
@@ -0,0 +1,15 @@
+let un = document.getElementById("email");
+let pass = document.getElementById("pass");
+let add = document.getElementById("address");
+let gd = document.getElementById("graduation");
+let branch = document.getElementById("branch");
+let yop = document.getElementById("yop");
+
+
+function registerUser()
+{
+
+	console.log(un);
+
+	
+}
